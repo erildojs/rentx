2e50af3c444a56e4fdf65668f9aac6b6ee752705
ErildoJS <erildofranciscojs@gmail.com>
2025-10-02 19:38:42 +0100

    chore: remove files

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata'
-import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
 import { app } from './app'
 
 app.listen(3333, () => {
